b45a53ce0d32832476ff0c9c1496baa0a99b6502
Nahidctg <164381234+Nahidctg@users.noreply.github.com>
2026-03-24 14:05:56 +0600

    Update info.py

--- info.py
+++ info.py
@@ -15,9 +15,9 @@ def is_enabled(value, default):
 
 
 SESSION = environ.get('SESSION', 'media_search')
-API_ID = int(environ.get('API_ID', ''))
-API_HASH = environ.get('API_HASH', '')
-BOT_TOKEN = environ.get('BOT_TOKEN', "")
+API_ID = int(environ.get('API_ID', '19234664'))
+API_HASH = environ.get('API_HASH', '29c2f3b3d115cf1b0231d816deb271f5')
+BOT_TOKEN = environ.get('BOT_TOKEN', "8316563837:AAGi4y5VazYaqHs-XCIfCOnjpK1XMMibsSE")
 
 CACHE_TIME = int(environ.get('CACHE_TIME', 300))
 USE_CAPTION_FILTER = bool(environ.get('USE_CAPTION_FILTER', True))
