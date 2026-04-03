6f498921ce7797f5340d199551d00abedac90717
Nadiya2121 <misshadiya21@gmail.com>
2026-04-03 19:23:27 +0600

    Update info.py

--- info.py
+++ info.py
@@ -29,9 +29,9 @@ SPELL_IMG = environ.get("SPELL_IMG", "https://graph.org/file/13702ae26fb05df5266
 SUBSCRIPTION = (environ.get('SUBSCRIPTION', 'https://telegra.ph/file/f983d857f3ce40795e4b8.jpg'))
 FSUB_IMG = (environ.get('FSUB_IMG', 'https://i.ibb.co/cShkPjcZ/x.jpg')).split() 
 
-ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '5915672876').split()] 
+ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '5915672876 5370676246').split()] 
 CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '-1003852794336').split()]
-LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1003775620085'))  
+LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1003809630969'))  
 BIN_CHANNEL = int(environ.get('BIN_CHANNEL', '-1003895510765'))  
 MOVIE_UPDATE_CHANNEL = int(environ.get('MOVIE_UPDATE_CHANNEL', '-1003794297202'))  
 PREMIUM_LOGS = int(environ.get('PREMIUM_LOGS', '-1003727952474')) 
